d27ef30a96d333482df7028ce1a2df5e1bcf6cb8
JetsonAI <96120477+jetsonai@users.noreply.github.com>
2024-08-24 20:02:42 +0900

    Update display_urdf.launch.py

--- launch/display_urdf.launch.py
+++ launch/display_urdf.launch.py
@@ -11,13 +11,8 @@ def generate_launch_description():
     use_sim_time = LaunchConfiguration("use_sim_time")
 
     pkg_path = os.path.join(get_package_share_directory("urdf_tutorial_ros2"))
-    #xacro_file = os.path.join(pkg_path, "urdf", "01-myfirst.urdf")
-    #xacro_file = os.path.join(pkg_path, "urdf", "02-multipleshapes.urdf")
-    #xacro_file = os.path.join(pkg_path, "urdf", "03-origins.urdf")
-    #xacro_file = os.path.join(pkg_path, "urdf", "04-materials.urdf")
-    #xacro_file = os.path.join(pkg_path, "urdf", "05-visual.urdf")
-    #xacro_file = os.path.join(pkg_path, "urdf", "06-flexible.urdf")
-    xacro_file = os.path.join(pkg_path, "urdf", "07-physics.urdf")
+    xacro_file = os.path.join(pkg_path, "urdf", "01-myfirst.urdf")
+
     robot_description = xacro.process_file(xacro_file)
     params = {"robot_description": robot_description.toxml(), "use_sim_time": use_sim_time}
 
